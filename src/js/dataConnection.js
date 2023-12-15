feb61eb645705be25fa2e4318135ed6cfaa8d228
IsmaGold67 <ismaelroberto67@gmail.com>
2023-12-15 16:55:53 -0400

    First Commit

---
+++ src/js/dataConnection.js
@@ -0,0 +1,79 @@
+export function showProjects(){
+    const aboutProject = document.querySelectorAll('.about-project');
+    const videoViewProject = document.querySelectorAll('.view-project');
+    fetch("./src/js/dados.json").then((response) => {
+        response.json().then((datas) => {
+            datas.videosProject.map((dataVideo) => {
+                showContentProject(dataVideo, aboutProject);
+                showVideoProject(dataVideo, videoViewProject);
+            });
+        });
+    });
+};
+
+export function showProjectsBackEnd(){
+    const aboutProject = document.querySelectorAll('.about-project');
+    const videoViewProject = document.querySelectorAll('.view-project');
+    fetch("./src/js/dados.json").then((response) => {
+        response.json().then((datas) => {
+            datas.videosProjectBackEnd.map((dataVideo) => {
+                showContentProject(dataVideo, aboutProject);
+                showVideoProject(dataVideo, videoViewProject);
+            });
+        });
+    });
+};
+
+function showContentProject(dataVideo, aboutProject){
+    aboutProject.forEach(element => {
+        const h1 = document.createElement("h1");
+        const h2 = document.createElement("h2");
+        const p = document.createElement("p");
+
+        if(element.classList.contains(dataVideo.id)){
+            h1.textContent = `Projeto ${dataVideo.id[4]}`;
+            h2.textContent = dataVideo.nomeProjeto;
+            p.textContent = dataVideo.description;
+
+            element.appendChild(h1);
+            element.appendChild(h2);
+            element.appendChild(p);
+        }
+    });
+}
+
+function showVideoProject(dataVideo, videoViewProject){
+    videoViewProject.forEach(element => {
+        const video = document.createElement('video');
+        const instruction = document.querySelectorAll('.instruction');
+        video.src = `${dataVideo.path}.mp4`;
+        video.autoplay = true;
+        video.muted = true;
+        video.loop = true;
+        video.onclick = redirectToDeploy; 
+        video.onmouseover = showInstructionMessage;
+        video.onmouseout = removeInstructionMessage;
+        if(element.classList.contains(dataVideo.id)){
+            element.appendChild(video);
+        }
+        function redirectToDeploy(){
+            window.location.href = dataVideo.deploy;
+        }
+        function showInstructionMessage(){
+            instruction.forEach(element => {
+                const ins = element.getAttribute('class');
+                if(ins[3] === dataVideo.id[4]){
+                    element.style.display = 'flex';
+                };
+            });
+        };
+        function removeInstructionMessage(){
+            instruction.forEach(element => {
+                const ins = element.getAttribute('class')
+                if(ins[3] === dataVideo.id[4]){
+                    element.style.display = 'none';
+                };
+            });
+        };
+    });
+};
---
+++ src/js/main.js
@@ -0,0 +1,93 @@
+import { showProjects } from "./dataConnection.js";
+import { showProjectsBackEnd } from "./dataConnection.js";
+const projectsPortfolio = document.querySelector('.projects-portfolio');
+const header = document.querySelector('.header');
+
+function apperHeaderBackgroundGradient(){
+    window.addEventListener('scroll', () => {
+        const y = window.pageYOffset;
+        if(y >= 100){
+            header.classList.add('gradient-header');
+        }else {
+            header.classList.remove('gradient-header');
+        };
+    });
+};
+
+function constructorDiv(){
+    var i = 0
+    for(i; i < 6; i++){
+        const divCard = document.createElement('div');
+        const divAbout = document.createElement("div");
+        const divView = document.createElement("div");
+        const divBefore = document.createElement("div");
+        const p = document.createElement('p');
+        p.textContent = `Clique no card para ser redirecionado!`;
+        p.classList.add(`ins${i + 1}`);
+        p.classList.add(`instruction`);
+    
+        divCard.classList.add("card-portfolio");
+        divAbout.classList.add(`proj${i + 1}`);
+        divView.classList.add(`proj${i + 1}`);
+        divAbout.classList.add("about-project");
+        divView.classList.add("view-project");
+        divBefore.classList.add("before");
+        divView.appendChild(p);
+    
+        divView.appendChild(divBefore);
+        divCard.appendChild(divAbout);
+        divCard.appendChild(divView);
+        projectsPortfolio.appendChild(divCard);
+    }    
+}
+
+function linkToNetworkHeader(){
+    const networks = document.querySelectorAll('.network-icons .fa-brands')
+    networks.forEach(network => {
+        network.addEventListener('click', () => {
+            const netArray = [
+                    'instagram',
+                    'ismagold67',
+                    'github',
+                    'ismagold67',
+                    'twitter',
+                    'ismelbateraa2',
+                    'linkedin',
+                    'in/ismael-roberto-586361212/',
+                    'youtube',
+                    'channel/UC_ghm_PU3W8dpa_4Jmk5saw'
+                    ];
+            const classNetwork = network.getAttribute('class');
+            for(var i = 0; i < netArray.length; i++){
+                if(classNetwork.includes(netArray[i])){
+                    window.location.href = `https://www.${netArray[i]}.com/${netArray[i + 1]}`;
+                };
+            };
+        });
+    });
+};
+
+function changeAProjectType(){
+    const front = document.querySelector('#front');
+    const back = document.querySelector('#back');
+
+    front.addEventListener('click', () => {
+        projectsPortfolio.innerHTML = ''
+        constructorDiv(); 
+        showProjects();
+    })
+    back.addEventListener('click', () => {
+        projectsPortfolio.innerHTML = ''
+        constructorDiv(); 
+        showProjectsBackEnd();
+    })
+}
+
+function init(){
+    apperHeaderBackgroundGradient();
+    constructorDiv(); 
+    showProjects();
+    changeAProjectType();
+    linkToNetworkHeader();
+}
+init();
